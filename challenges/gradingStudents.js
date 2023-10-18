937471c566d3a27f0899707610c60e4467c9f994
J Rafael <rafaelsilva9240@hotmail.com>
2023-10-17 22:56:16 -0300

    desafio de notas dos estudantes

--- challenges/gradingStudents.js
+++ challenges/gradingStudents.js
@@ -1,31 +1,27 @@
 function gradingStudents(grades) {
-  if (grades.length >= 1 || grades.length <= 60) {
-    const notas = grades.map((nota) => {
-      if (nota >= 0 || nota <= 100) {
-        const notaArredondada = Math.ceil(nota / 5);
-        const proximoMultiploDeCinco = notaArredondada * 5;
-        const diferençaEntreNotaEProximoMultiploDeCinco = proximoMultiploDeCinco - nota
+    if (grades.length >= 1 || grades.length <= 60) {
+        const notas = grades.map((nota) => {
+        if (nota >= 0 || nota <= 100) {
+            const notaArredondada = Math.ceil(nota / 5);
+            const proximoMultiploDeCinco = notaArredondada * 5;
+            const diferencaEntreNotaEProximoMultiploDeCinco = proximoMultiploDeCinco - nota
 
+            let valorDaNota;
 
-        let valorDaNota;
-
-        if (nota < 38 || proximoMultiploDeCinco < 40) {
-          valorDaNota = nota;
-        } else {
-          if (diferençaEntreNotaEProximoMultiploDeCinco < 3) {
-              valorDaNota = proximoMultiploDeCinco;
+            if (nota < 38 || proximoMultiploDeCinco < 40) {
+            valorDaNota = nota;
             } else {
-              valorDaNota = nota;
-          }
+            if (diferencaEntreNotaEProximoMultiploDeCinco < 3) {
+                valorDaNota = proximoMultiploDeCinco;
+                } else {
+                valorDaNota = nota;
+            }
+            }
+            return valorDaNota;
         }
-        return valorDaNota;
-      }
-    });
-    console.log("notas", notas);
-    return notas;
-  }
-
-  // Write your code here
+        });
+        return notas;
+    }
 }
 
 const grades = [
